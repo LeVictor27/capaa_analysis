1fe58ac6034e5084902b841dbbd4e862dd5404b2
Katherine Chang <kachang@uw.edu>
2022-01-07 01:51:23 -0800

    Adjusting B for slide deck + theme_uw update

--- theme_uw.R
+++ theme_uw.R
@@ -1,8 +1,3 @@
-
-#palette_uw <- colorRampPalette(c("#4B2E83", "#E8E3D3"))
-#palette_uw_reverse<- colorRampPalette(c("#E8E3D3", "#4B2E83"))
-#palette_black_white <- colorRampPalette(c("#FFFFFF", "#000000"))
-
 theme_uw <- function(){ 
   #Adapted from Maddie Picken's RPubs/RStudio Tutorial
   #https://rpubs.com/mclaire19/ggplot2-custom-themes
@@ -11,42 +6,96 @@ theme_uw <- function(){
   theme_bw() %+replace%    #replace elements we want to change
     theme(
       #grid elements
-      #panel.grid.major = element_blank(),    #strip major gridlines
-      #panel.grid.minor = element_blank(),    #strip minor gridlines
-      #axis.ticks = element_blank(),          #strip axis ticks
+      panel.border = element_blank(),
+      panel.background = element_blank(),
+      strip.background=element_blank(),
+      panel.grid.major = element_blank(),    
+      panel.grid.minor = element_blank(),    
+      panel.grid.major.x = element_line(color = "#D3D3D3"), 
+      legend.title = element_blank(), 
+      #Removing legend title since it usually defaults to variable name,
+      #manually define it
       
-      #text elements
-      plot.title = element_text(             #title
+      plot.title = element_text(           
+        family = font,           
+        size = 16,                
+        #face = 'bold',           
+        margin=margin(5, b = 10),  
+        hjust = 0.5),                #Center align
+                       
+      plot.subtitle = element_text(         
+        family = font,  
+        size = 10,
+        margin=margin(5, b = 10),  
+        ),               
+      
+      plot.caption = element_text(          
+        family = font,
+        face="italic",
+        size = 9,              
+        hjust = 0,  #left justified
+        margin=margin(5, b = 10)),               
+      
+      axis.title = element_text(             
+        family = font,           
+        size = 10),             
+      
+      axis.text = element_text(             
+        family = font,         
+        size = 9),               
+      
+      axis.text.x = element_text(           
+        margin=margin(5, b = 10))
+    )
+}
+
+theme_uw_map <- function(){ 
+  #Adapted from Maddie Picken's RPubs/RStudio Tutorial
+  #https://rpubs.com/mclaire19/ggplot2-custom-themes
+  
+  font <- "Arial"   #assign font family up front
+  theme_void() %+replace%    #replace elements we want to change
+    theme(
+      
+      panel.border = element_blank(),
+      panel.background = element_blank(),
+      strip.background=element_blank(),
+      panel.grid.major = element_blank(),    
+      panel.grid.minor = element_blank(),    
+      axis.ticks = element_blank(),         
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      rect = element_blank(),
+      
+      panel.grid.major.x = element_line(color = "#D3D3D3"), #add back x gridlines
+      legend.title = element_blank(), 
+      #Removing legend title since it usually defaults to variable name,
+      #manually define it
+      
+      plot.title = element_text(         
         family = font,           
         size = 16,                
         #face = 'bold',           
         margin=margin(5, b = 10),  #Margins around title
         hjust = 0.5),                #Center align
-                       
       
-      plot.subtitle = element_text(          #subtitle
-        family = font,            
-        size = 14,
+      plot.subtitle = element_text(         
+        family = font,  
+        size = 10,
         margin=margin(5, b = 10),  #Margins around title
-        ),               
+      ),               
       
-      plot.caption = element_text(           #caption
-        family = font,          
+      plot.caption = element_text(          
+        family = font,
+        face="italic",
         size = 9,              
-        hjust = 1),               #right align
+        hjust = 0,  #left justified
+        margin=margin(5, b = 10)),               
       
-      axis.title = element_text(             #axis titles
+      axis.title = element_text(             
         family = font,           
-        size = 10),             
+        size = 10)
       
-      axis.text = element_text(              #axis text
-        family = font,         
-        size = 9),               
-      
-      axis.text.x = element_text(            #margin for axis text
-        margin=margin(5, b = 10))
-      
-      #since the legend often requires manual tweaking 
-      #based on plot content, don't define it here
+
     )
 }
